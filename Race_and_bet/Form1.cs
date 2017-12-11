d88bf8305d2e3d3f870944395059616114a5546a
TomaszGrzesik <t.grzesik19@gmail.com>
2017-12-11 13:41:09 +0100

    in process

---
+++ Race_and_bet/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Race_and_bet
+{
+    public partial class Form1 : Form
+    {
+        Hound[] dogs = new Hound[4];
+        Guy[] guys = new Guy[3];
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetupRaceTrack();
+        }
+
+        private void SetupRaceTrack()
+        {
+            label2.Text = "Minimum Bet " + numericUpDown1;
+
+            int StartingPoint = pictureBox2.Right - pictureBox1.Right;
+            int TrackLenght = pictureBox1.Size.Width;
+
+            dogs[0] = new Hound()
+            {
+                MyPictureBox = pictureBox2,
+                RaceTrackLenght = TrackLenght,
+                StartingPosition = StartingPoint
+            };
+
+            dogs[1] = new Hound()
+            {
+                MyPictureBox = pictureBox3,
+                RaceTrackLenght = TrackLenght,
+                StartingPosition = StartingPoint
+            };
+
+            dogs[2] = new Hound()
+            {
+                MyPictureBox = pictureBox4,
+                RaceTrackLenght = TrackLenght,
+                StartingPosition = StartingPoint
+            };
+
+            dogs[3] = new Hound()
+            {
+                MyPictureBox = pictureBox5,
+                RaceTrackLenght = TrackLenght,
+                StartingPosition = StartingPoint
+            };
+
+            guys[0] = new Guy( "Joe", null, 50, radioButton1, textBox1);
+            guys[1] = new Guy("Bob", null, 75, radioButton2, textBox2);
+            guys[2] = new Guy("John", null, 5, radioButton3, textBox3);
+
+            foreach( Guy guy in guys)
+            {
+                guy.UpdateLabels();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool NoWinner = true;
+            int WinningDog;
+            button1.Enabled = false;
+
+            while (NoWinner)
+            {
+                Application.DoEvents();
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int GuyNumber = 0;
+
+            if(radioButton1.Checked)
+            {
+                GuyNumber = 0;
+            }
+
+            if(radioButton2.Checked)
+            {
+                GuyNumber = 1;
+            }
+
+            if(radioButton3.Checked)
+            {
+                GuyNumber = 2;
+            }
+
+            guys[GuyNumber].PlaceBet((int)numericUpDown1.Value, (int)numericUpDown2.Value);
+            guys[GuyNumber].UpdateLabels();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            SetBettorName("Joe");
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            SetBettorName("Bob");
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            SetBettorName("John");
+        }
+
+        private void SetBettorName(string Name)
+        {
+            label4.Text = Name;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i< 3; i++)
+            {
+                if (dogs[i] != null)
+                {
+                    dogs[i].Run();
+                }
+
+            }
+        }
+    }
+}
---
+++ Race_and_bet/Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Race_and_bet
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+
+    public class Guy
+    {
+        public string Name;
+        public Bet MyBet;
+        public int Cash;
+
+        public RadioButton MyRadioButton;
+        public Label MyLabel;
+        public TextBox MyTextBox;
+
+        public Guy(string Name, Bet MyBet, int Cash, RadioButton MyRadioButton, TextBox MyTextBox)
+        {
+            this.Name = Name;
+            this.MyBet = MyBet;
+            this.Cash = Cash;
+            this.MyRadioButton = MyRadioButton;
+            this.MyTextBox = MyTextBox;
+        }
+
+        public void UpdateLabels()
+        {
+            if (MyBet == null)
+            {
+                MyTextBox.Text = Name + " hasn't placed any bets";
+            }
+            else
+            {
+                MyRadioButton.Text = Name + " bets " + Cash + "$";
+            }
+
+        }
+
+        public void ClearBet()
+        {
+            MyBet.Amount = 0;
+        }
+
+        public bool PlaceBet(int Amount, int Dog)
+        {
+            if (Amount <= Cash)
+            {
+                MyBet = new Bet(Amount, Dog, this);
+                return true;
+            }
+            return false;
+        }
+
+        public void Collect(int Winner)
+        {
+            Cash += MyBet.PayOut(Winner);
+        }
+
+    }
+
+    public class Hound
+    {
+        public int StartingPosition; //place where race begin 
+        public int RaceTrackLenght; // lenght of race track
+        public PictureBox MyPictureBox = null; // object which gonna race
+        public int location = 0; // location on track
+        public Random MyRandom; // class instance Random
+        public bool finish;
+
+        public bool Run()
+        {
+             MyRandom = new Random();
+
+            if (MyPictureBox != null)
+            {
+                StartingPosition += location.Next(1, 5);
+                ///location += MyRandom.Next(1, 4); // go random 1, 2, 3 or 4 points
+            }
+            //return (MyPictureBox.Right >= RaceTrackLenght - MyPictureBox.Width);
+            return true;
+        }
+
+        public void TakeStartingPosition()
+        {
+            StartingPosition = 0;            
+        }
+    }
+
+    public class Bet
+    {
+        public int Amount;
+        public int Dog;
+        public Guy Bettor;
+
+        public Bet(int Amount, int Dog, Guy Bettor)
+        {
+            this.Amount = Amount;
+            this.Dog = Dog;
+            this.Bettor = Bettor;
+        }
+
+        public string GetDescription()
+        {
+            string description = " ";
+            if (Amount != 0)
+            {
+                description += Bettor.Name + " bets " + Amount + " at dog no. " + Dog;
+            }
+            else
+            {
+                description += Bettor.Name + " hasn't placed any bets.";
+            }
+            return description;
+
+        }
+        
+        public int PayOut(int Winner)
+        {
+            if (Dog == Winner)
+            {
+                return Amount;
+            } 
+            else
+            {
+                return -Amount;
+            }
+
+        }
+    }
+}
